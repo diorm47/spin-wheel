e7675d504a20ffaec5eb733361f516e962ef7285
dior <dior47m@gmail.com>
2022-11-21 17:31:41 +0500

    added aos

--- src/components/winned-sum/winned.jsx
+++ src/components/winned-sum/winned.jsx
@@ -1,18 +1,33 @@
-import React from "react";
-import "./winned.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import React, { useEffect } from "react";
 import salute1 from "../../assets/salute1.gif";
 import salute2 from "../../assets/salute2.gif";
+import "./winned.css";
 
 function Winned({ winnedSum, setModal }) {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <div className="winned_wrapper">
       <div className="winned_texts">
-        <div className="winned_summ">
+        <div
+          className="winned_summ"
+          data-aos="fade-down"
+          data-aos-duration="1000"
+        >
           <h1>
             You win: <span> {winnedSum} </span>
           </h1>
         </div>
-        <div className="retry_btn" onClick={() => setModal(false)}>
+        <div
+          className="retry_btn"
+          data-aos="fade-down"
+          data-aos-duration="1000"
+          onClick={() => setModal(false)}
+        >
           <p>Spin</p>
         </div>
 
